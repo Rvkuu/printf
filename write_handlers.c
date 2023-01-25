871120aaae3dbc578c131fc2727ae02650c1f9d6
rvkuu <preciousubi.sam@gmail.com>
2023-01-24 20:39:39 -0800

    Printf Update

--- write_handlers.c
+++ write_handlers.c
@@ -38,7 +38,7 @@ int handle_write_char(char c, char buffer[]
 			return (write(1, &buffer[0], 1) +
 					write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
 		else
-			return (write(1, &buffer[BUFF_SIZE -i - 1], width - 1) +
+			return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
 					write(1, &buffer[0], 1));
 	}
 
@@ -136,7 +136,7 @@ int write_num(int ind, char buffer[],
 		}
 	}
 	if (extra_c)
-		buffer[==ind] = extra_c;
+		buffer[--ind] = extra_c;
 	return (write(1, &buffer[ind], length));
 }
 
@@ -243,4 +243,11 @@ int write_pointer(char buffer[], int ind, int length,
 			buffer[2] = 'x';
 			return (write(1, &buffer[padd_start], i - padd_start) +
 					write(1, &buffer[ind], length - (1 - padd_start) - 2));
-
+		}
+	}
+	buffer[--ind] = 'x';
+	buffer[--ind] = '0';
+	if (extra_c)
+		buffer[--ind] = extra_c;
+	return (write(1, &buffer[ind], BUFF_SIZE - ind - 1));
+}
